edbff2417a89e8c605de9cfe144aab1f029d9412
Mateus Borges <spekytro68@gmail.com>
2023-02-19 07:59:17 -0300

    ajustes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,13 +5,12 @@ import { postRoutes } from "./post.routes";
 import { postGroupRoutes } from "./post_group.routes";
 
 import { userRoutes } from "./user.routes";
-import { UserPostRoutes } from "./user_post.routes";
+
 const routes = Router();
 
 routes.use("/users", userRoutes);
 routes.use("/posts", postRoutes);
 routes.use("/groups", groupRoutes);
 routes.use("/postgroups", postGroupRoutes);
-routes.use("/userpost", UserPostRoutes);
 
 export { routes };
