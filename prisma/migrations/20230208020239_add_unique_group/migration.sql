c41e2bece2f005599a657c6713bcd57ad0840f2d
Mateus Borges <spekytro68@gmail.com>
2023-02-19 02:54:54 -0300

    ajustes

--- prisma/migrations/20230208020239_add_unique_group/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name_group]` on the table `group` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "group_name_group_key" ON "group"("name_group");
--- prisma/migrations/20230219054309_create_db/migration.sql
+++ prisma/migrations/20230219054309_create_db/migration.sql
@@ -15,8 +15,8 @@ CREATE TABLE "post" (
     "title" TEXT NOT NULL,
     "subtitle" TEXT NOT NULL,
     "subject" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "url_photo" TEXT NOT NULL,
+    "date_post" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "post_pkey" PRIMARY KEY ("id")
 );
@@ -24,8 +24,9 @@ CREATE TABLE "post" (
 -- CreateTable
 CREATE TABLE "group" (
     "id" TEXT NOT NULL,
-    "sigla_group" TEXT NOT NULL,
+    "name_group" TEXT NOT NULL,
     "description" TEXT NOT NULL,
+    "color_hex" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -34,18 +35,20 @@ CREATE TABLE "group" (
 
 -- CreateTable
 CREATE TABLE "post_group" (
+    "id" TEXT NOT NULL,
     "groupId" TEXT NOT NULL,
     "postId" TEXT NOT NULL,
 
-    CONSTRAINT "post_group_pkey" PRIMARY KEY ("groupId","postId")
+    CONSTRAINT "post_group_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "post_user" (
+CREATE TABLE "user_post" (
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "postId" TEXT NOT NULL,
 
-    CONSTRAINT "post_user_pkey" PRIMARY KEY ("userId","postId")
+    CONSTRAINT "user_post_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -55,7 +58,22 @@ CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
 CREATE UNIQUE INDEX "post_title_key" ON "post"("title");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "group_sigla_group_key" ON "group"("sigla_group");
+CREATE UNIQUE INDEX "group_id_key" ON "group"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "group_name_group_key" ON "group"("name_group");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "post_group_id_key" ON "post_group"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "post_group_postId_key" ON "post_group"("postId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_post_id_key" ON "user_post"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_post_postId_key" ON "user_post"("postId");
 
 -- AddForeignKey
 ALTER TABLE "post_group" ADD CONSTRAINT "post_group_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "group"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -64,7 +82,7 @@ ALTER TABLE "post_group" ADD CONSTRAINT "post_group_groupId_fkey" FOREIGN KEY ("
 ALTER TABLE "post_group" ADD CONSTRAINT "post_group_postId_fkey" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "post_user" ADD CONSTRAINT "post_user_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "user_post" ADD CONSTRAINT "user_post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "post_user" ADD CONSTRAINT "post_user_postId_fkey" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "user_post" ADD CONSTRAINT "user_post_postId_fkey" FOREIGN KEY ("postId") REFERENCES "post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
