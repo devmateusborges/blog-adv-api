978acb4cf645fe20409c57ba6c302631d332ad87
Mateus Borges <spekytro68@gmail.com>
2023-03-11 00:23:57 -0300

    AJUSTES

--- src/modules/post/useCases/getPostByDate/CountPostUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "../../../../prisma/client";
-
-export class GetCountPostUseCase {
-  async execute(): Promise<number> {
-    const countPost = await prisma.post.count();
-
-    return countPost;
-  }
-}
--- src/modules/post/useCases/getPostByDate/GetPostByDateController.ts
+++ src/modules/post/useCases/getPostByDate/GetPostByDateController.ts
@@ -1,18 +1,16 @@
 import { Request, Response } from "express";
-import { GetCountPostUseCase } from "./CountPostUseCase";
 import { GetPostByDateUseCase } from "./GetPostByDateUseCase";
 
 export class GetPostByDateController {
   async handle(req: Request, res: Response) {
     const getPostByDateUseCase = new GetPostByDateUseCase();
-    const getCountPostUseCase = new GetCountPostUseCase();
+
     const result = await getPostByDateUseCase.execute();
-    const count = await getCountPostUseCase.execute();
 
     return res.status(200).json({
       code: 200,
       items: result,
-      total_items: count,
+      total_items: 0,
     });
   }
 }
