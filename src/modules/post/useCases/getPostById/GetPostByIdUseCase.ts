8bfaf40cb19d8c85c68f953b523a8f8879f6ed1a
Mateus Borges <spekytro68@gmail.com>
2023-03-13 21:20:47 -0300

    tst

--- src/modules/post/useCases/getPostById/GetPostByIdUseCase.ts
+++ src/modules/post/useCases/getPostById/GetPostByIdUseCase.ts
@@ -5,10 +5,6 @@ export class GetPostByIdUseCase {
   async execute(id: string): Promise<Post[]> {
     const post = await prisma.post.findMany({
       where: { id: id },
-      include: {
-        group: { select: { name_group: true, description: true } },
-        user: { select: { email: true, name: true } },
-      },
     });
 
     return post;
