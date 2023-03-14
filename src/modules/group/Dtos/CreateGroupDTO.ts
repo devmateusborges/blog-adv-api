c67cc3a736fb33be0948c724807ad48a905daa3f
Mateus Borges <spekytro68@gmail.com>
2023-03-14 19:43:14 -0300

    ajustes

--- src/modules/group/Dtos/CreateGroupDTO.ts
+++ src/modules/group/Dtos/CreateGroupDTO.ts
@@ -1,7 +1,18 @@
 export interface CreateGroupDTO {
   id?: string;
+  id_group_1?: string;
   name_group: string;
   description: string;
   color_hex: string;
+  post?: PostDTO[];
   user_id: string;
+  title?: string;
+  subtitle?: string;
+  group_id?: string;
+}
+
+export interface PostDTO {
+  id?: string;
+  title: string;
+  subtitle: string;
 }
--- src/modules/group/useCases/getGroupAll/GetAllGroupUseCase.ts
+++ src/modules/group/useCases/getGroupAll/GetAllGroupUseCase.ts
@@ -1,18 +1,50 @@
 import { Group, Post } from "@prisma/client";
 
 import { prisma } from "../../../../prisma/client";
+import { CreateGroupDTO } from "../../Dtos/CreateGroupDTO";
 
 export class GetAllGroupUseCase {
   async execute(): Promise<any[]> {
-    const query2 = await prisma.group.findMany({
-      orderBy: {
-        created_at: "asc",
-      },
-      select: {
-        _count: true,
-      },
+    const query1: Group[] =
+      await prisma.$queryRaw`SELECT a.id as "id_group_1" , a.name_group , a.description, b.id , b.title ,b.subtitle  , b.group_id 
+      FROM "group" a
+      INNER JOIN "post" b
+      on a.id  = b.group_id  ;`;
+
+    const res: any[] | PromiseLike<any[]> = [];
+    const post: any[] | PromiseLike<any[]> = [];
+
+    let id: string | undefined;
+    query1.map((group: CreateGroupDTO) => {
+      post.push({
+        id: group.id,
+        title: group.title,
+        subtitle: group.subtitle,
+        group_id: group.group_id,
+      });
     });
 
-    return query2;
+    query1.map((group: CreateGroupDTO) => {
+      if (group.id_group_1 != id) {
+        res.push({
+          id: group.id_group_1,
+          name_group: group.name_group,
+          post: post.filter((post) => post.group_id == group.id_group_1),
+        });
+        id = group.id_group_1;
+      }
+    });
+
+    return res;
+    // const query2 = await prisma.group.findMany({
+    //   orderBy: {
+    //     created_at: "asc",
+    //   },
+    //   select: {
+    //     _count: true,
+    //   },
+    // });
+
+    // return query2;
   }
 }
