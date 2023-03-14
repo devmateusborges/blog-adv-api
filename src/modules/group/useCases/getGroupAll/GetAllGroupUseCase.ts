5a4b88a6f1b2af92a3c8e76b41e20520438c4bad
Mateus Borges <spekytro68@gmail.com>
2023-03-14 14:56:11 -0300

    teste 2

--- src/modules/group/useCases/getGroupAll/GetAllGroupUseCase.ts
+++ src/modules/group/useCases/getGroupAll/GetAllGroupUseCase.ts
@@ -3,13 +3,19 @@ import { Group, Post } from "@prisma/client";
 import { prisma } from "../../../../prisma/client";
 
 export class GetAllGroupUseCase {
-  async execute(): Promise<Group[]> {
+  async execute(): Promise<any[]> {
     const query2 = await prisma.group.findMany({
       orderBy: {
         created_at: "asc",
       },
-      include: {
-        post: true,
+      select: {
+        post: {
+          select: {
+            id: true,
+            title: true,
+            subtitle: true,
+          },
+        },
       },
     });
 
